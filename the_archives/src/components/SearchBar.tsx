e8f9dbb0b2ad7110575aa2e025124fbc93985047
Samuel <124836035+samzeh@users.noreply.github.com>
2026-02-22 16:59:19 -0500

    added stuff

---
+++ the_archives/src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/searchbar.css'
+
+const SearchBar = () => {
+  return (
+    <>
+      <input type="text" placeholder="Search the archives..." className = "search-input" />
+      
+    </>
+  )
+}
+
+export default SearchBar
---
+++ the_archives/src/components/SearchBarButton.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/searchbar.css'
+import { BsSearch } from "react-icons/bs";
+
+
+
+
+const SearchBarButton = () => {
+  return (
+    <>
+      <div className="search-button">
+        <BsSearch />
+      </div>
+      
+    </>
+  )
+}
+
+export default SearchBarButton
