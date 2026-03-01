4af5ae0de6ef323d939c68dc80f0ecc8dd983282
Samuel <124836035+samzeh@users.noreply.github.com>
2026-02-28 21:12:45 -0500

    added searchresults to searchbar

--- the_archives/src/components/SearchBar.tsx
+++ the_archives/src/components/SearchBar.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import '../styles/footer.css'
 import { BsSearch } from "react-icons/bs";
+import { useState} from 'react';
+import SearchResults from './SearchResults';
 
 
 export default function SearchBar() {
+  const [query, setQuery] = useState('')
+
   return (
-    <div className="search-bar">
-      <BsSearch style={{ color: '#847058', flexShrink: 0 }} />
-      <input className="search-input" placeholder="Search for book title..." />
+    <div className="search-bar-wrapper">
+      {query && (
+        <SearchResults />
+      )}
+      <div className="search-bar">
+        <BsSearch style={{ color: '#847058', flexShrink: 0 }} />
+        <input 
+          className="search-input" 
+          placeholder="Search for book title..." 
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
     </div>
   )
 }
