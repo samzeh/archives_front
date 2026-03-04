5d706ee91673eabdd9bc6a58df167d2bb03509d6
Samuel <124836035+samzeh@users.noreply.github.com>
2026-03-03 21:05:14 -0500

    changes

---
+++ the_archives/src/components/DefaultHomeComponents.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/login.css'
+
+export default function DefaultHomeComponents()  {
+  return (
+    <div className="action-links">
+      <p>login</p>
+      <p>sign up</p>
+      <p>guest</p>
+    </div>      
+  )
+}
+
--- the_archives/src/components/HomeComponent.tsx
+++ the_archives/src/components/HomeComponent.tsx
@@ -2,17 +2,14 @@ import React from 'react'
 import '../styles/login.css'
 import LoginComponent from './LoginComponent.tsx'
 import SignupComponent from './SignupComponent.tsx'
+import DefaultHomeComponents from './DefaultHomeComponents.tsx'
 
 export default function HomeComponent() {
   return (
     <div className = "home-box">
       <h1 className="welcome-text"> the archives</h1>
-      {/* <div className="action-links">
-        <p>login</p>
-        <p>sign up</p>
-        <p>guest</p>
-      </div>       */}
-      <LoginComponent />
+      {/* <LoginComponent /> */}
+      <DefaultHomeComponents />
       {/* <SignupComponent /> */}
     </div>
   )
