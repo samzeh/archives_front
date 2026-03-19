48140e569cb0baa37072816a034d821fbe8ff39f
Samuel <124836035+samzeh@users.noreply.github.com>
2026-03-19 11:22:28 -0400

    fix path

--- backend/model-backend/download_artifact.py
+++ backend/model-backend/download_artifact.py
@@ -2,7 +2,7 @@ import gdown
 import os
 
 url = "https://drive.google.com/uc?export=download&id=1AlW2r1rAkI4nAMKQVzp1-WrUIIqSx-UD"
-output_path = "notebooks/artifacts.pkl.gz"
+output_path = "data/processed/artifacts.pkl.gz"
 
 os.makedirs(os.path.dirname(output_path), exist_ok=True)
 gdown.download(url, output_path, quiet=False)
